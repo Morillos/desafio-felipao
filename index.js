56c46a98d210c1bd0bd90c28a1b88cb3b6ed34fc
Morillos <murilobarcelos103@gmail.com>
2024-07-22 14:23:04 -0300

    indentação melhorada

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 let nomeHeroi = "Morillos";
-let xpHeroi = 1005;
+let xpHeroi = 8250;
 let nivelHeroi = "";
 
-if (xpHeroi < 1000) {
-  nivelHeroi = "Ferro";
-} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
-  nivelHeroi = "Bronze";
-} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-  nivelHeroi = "Prata";
-} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
-  nivelHeroi = "Ouro";
-} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
-  nivelHeroi = "Platina";
-} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-  nivelHeroi = "Ascendente";
-} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-  nivelHeroi = "Imortal";
-} else {
-  nivelHeroi = "Radiante";
-}
+if (xpHeroi < 1000) {nivelHeroi = "Ferro";} 
+if (xpHeroi >= 1001 && xpHeroi <= 2000) {nivelHeroi = "Bronze";} 
+if (xpHeroi >= 2001 && xpHeroi <= 5000) {nivelHeroi = "Prata";}  
+if (xpHeroi >= 5001 && xpHeroi <= 7000) {nivelHeroi = "Ouro";}
+if (xpHeroi >= 7001 && xpHeroi <= 8000) {nivelHeroi = "Platina";}  
+if (xpHeroi >= 8001 && xpHeroi <= 9000) {nivelHeroi = "Ascendente"}  
+if (xpHeroi >= 9001 && xpHeroi <= 10000) {nivelHeroi = "Imortal";} 
+if (nivelHeroi > 10000) {nivelHeroi = "Radiante";}
 
 console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi +".");
